1d25dc8414856b866cb16d33f0bdceeccd0e7f34
Federico Cangiotti <federico.cangiotti05@gmail.com>
2022-11-10 08:25:27 +0100

    feat: Aggiunge metodo MediaPonderata

--- AS2223_4G_INF_CangiottiFederico_Statistiche/Statistiche.cs
+++ AS2223_4G_INF_CangiottiFederico_Statistiche/Statistiche.cs
@@ -56,7 +56,7 @@ namespace AS2223_4G_INF_CangiottiFederico_Statistiche
         /// <returns></returns>
         public double MediaPonderata()
         {
-            return 0;
+            return sommaValoriPerPeso / sommaPesi;
         }
 
         /// <summary>
