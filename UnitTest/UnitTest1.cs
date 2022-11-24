5b714b06452178c0ef0e854a63df9546d0a36ba2
Federico Cangiotti <federico.cangiotti05@gmail.com>
2022-11-24 08:50:45 +0100

    feat: Implementa unit test MediaAritmetica

--- UnitTest/UnitTest1.cs
+++ UnitTest/UnitTest1.cs
@@ -1,3 +1,5 @@
+using AS2223_4G_INF_CangiottiFederico_Statistiche;
+
 namespace UnitTest
 {
     public class Tests
@@ -10,7 +12,18 @@ namespace UnitTest
         [Test]
         public void Test_MediaAritmetica()
         {
-            Assert.Pass();
+            Statistiche s = new Statistiche();
+
+            s.AddValue(15);
+            s.AddValue(6);
+            s.AddValue(7);
+            s.AddValue(18);
+            s.AddValue(9);
+            s.AddValue(100);
+            s.AddValue(114);
+            s.AddValue(121);
+
+            Assert.IsTrue(s.MediaAritmetica() == 48.75);
         }
 
         [Test]
